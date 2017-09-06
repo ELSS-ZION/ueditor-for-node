50dbb0fafc02448656b666c71063b42e6bfdd006
ELSS <elsszion@gmail.com>
2017-09-06 23:46:54 +0800

    neditor

---
+++ example/neditor/app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const ueditor_backend = require('ueditor-backend')
+const app = express()
+ueditor_backend(app)
+app.use(express.static('utf8-php'))
+app.listen(80)
